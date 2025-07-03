23f5f442e041fdf085c8163bb41d960c1c2dbc20
pongpiphat-ka <pongpiphat_k@cmu.ac.th>
2025-07-03 22:19:03 +0700

    7. update EventCard.vue component

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,42 +1,36 @@
 <script setup lang="ts">
-// defineProps<{
-//   msg: string
-// }>()
+import { ref } from 'vue';
+const event = ref({
+ id: 5928101,
+ category: 'animal welfare',
+ title: 'Cat Adoption Event',
+  description: 'Find your new feline friend at this event.',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
-  <!-- <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div> -->
-  <div class = "greetings"></div>
+ <div class="event-class">
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
+ </div>
 </template>
 
-<!-- <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+<style scoped>
+.event-card {
+ padding: 20px;
+ width: 250px;
+ cursor: pointer;
+ border: 1px solid #39496c;
+ margin-bottom: 18px;
 }
-
-h3 {
-  font-size: 1.2rem;
+.event-card:hover {
+ transform: scale(1.01);
+ box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style> -->
+</style> 
--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+// Update the path below if EventCard.vue is in a different folder
+import EventCard from '../components/EventCard.vue';
 </script>
 
 <template>
-<div class="home"></div>
+<div class="home">
+  <EventCard />
+  </div>
 </template>
