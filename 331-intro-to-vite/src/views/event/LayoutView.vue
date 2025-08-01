97e2be6d06f57624f944a3ef2b37b9302735e978
pongpiphat-ka <pongpiphat_k@cmu.ac.th>
2025-08-01 09:43:42 +0700

    fix nav LayoutView

--- 331-intro-to-vite/src/views/event/LayoutView.vue
+++ 331-intro-to-vite/src/views/event/LayoutView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import { RouterLink, RouterView,useRouter } from 'vue-router'
 import { useEventStore } from '@/stores/event'
 import { storeToRefs } from 'pinia'
 
@@ -11,11 +12,11 @@ const { event } = storeToRefs(store)
   <div v-if="event">
     <h1>{{ event.title }}</h1>
     <nav>
-        <Router-link :to="{ name: 'event-detail-view'}">Details 
+        <Router-link exact-active-class="text-green-500" :to="{ name: 'event-detail-view'}">Details 
         </Router-link>  | 
-        <Router-link :to="{ name: 'event-register-view'}">Register
+        <Router-link exact-active-class="text-green-500" :to="{ name: 'event-register-view'}">Register
         </Router-link> |
-        <Router-link :to="{ name: 'event-edit-view'}">Edit</Router-link>
+        <Router-link exact-active-class="text-green-500" :to="{ name: 'event-edit-view'}">Edit</Router-link>
     </nav>
     <RouterView :event="event" />
     </div>
